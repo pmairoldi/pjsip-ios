eff6a1c92ee8e51d97bb3554c986f76a4a3cc42f
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2014-09-03 15:22:57 -0400

    test transport

--- Example/pjsip-ios/PMAppDelegate.m
+++ Example/pjsip-ios/PMAppDelegate.m
@@ -60,6 +60,17 @@
     pjsua_media_config_default(&media_cfg);
     
     pjsua_init(&ua_cfg, &log_cfg, &media_cfg);
+    
+    pjsua_transport_config transportConfig;
+    
+    pjsua_transport_config_default(&transportConfig);
+    
+    transportConfig.port = 5060;
+    
+    pjsua_transport_create(PJSIP_TRANSPORT_UDP, &transportConfig, NULL);
+    pjsua_transport_create(PJSIP_TRANSPORT_TCP, &transportConfig, NULL);
+
+    pjsua_start();
 }
 
 @end
